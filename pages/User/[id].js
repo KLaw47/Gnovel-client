0929502c6c49a286d521f57902826b7f97eb9839
Keaton Law <keaton.c.law@gmail.com>
2023-03-04 09:23:46 -0600

    conditional for remove from collection

--- pages/User/[id].js
+++ pages/User/[id].js
@@ -6,11 +6,13 @@ import { Button } from 'react-bootstrap';
 import ComicCard from '../../components/ComicCard';
 import { getSingleUser } from '../../utils/data/userData';
 import { deleteUserComic } from '../../utils/data/userComicData';
+import { useAuth } from '../../utils/context/authContext';
 
 export default function UserComics() {
   const [user, setUser] = useState({});
   const router = useRouter();
   const { id } = router.query;
+  const fUser = useAuth();
 
   const getThisUser = () => {
     getSingleUser(id).then(setUser);
@@ -25,7 +27,7 @@ export default function UserComics() {
     getThisUser();
   }, [id]);
 
-  console.warn(user);
+  console.warn(fUser.user.id);
 
   return (
     <div>
@@ -41,9 +43,11 @@ export default function UserComics() {
         {user?.comics?.map((comic) => (
           <div key={comic.id}>
             <ComicCard comicObj={comic} />
-            <Button key={comic.joined_comics[0].id} className="custom-btn" onClick={(click) => removeFromCollection(click, comic.joined_comics[0].id)}>
-              Remove from my Collection
-            </Button>
+            {user?.id === fUser.user.id ? (
+              <Button key={comic.joined_comics[0].id} className="custom-btn" onClick={(click) => removeFromCollection(click, comic.joined_comics[0].id)}>
+                Remove from my Collection
+              </Button>
+            ) : null}
           </div>
 
         ))}
